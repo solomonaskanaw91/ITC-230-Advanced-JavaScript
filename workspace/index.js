a039b16becf3f6df6aa462c3647f756b61806ca8
Solomon <solomon.ab1988@gmail.com>
2018-06-29 11:38:01 -0700

    ITC230 Week 1 App
    This week I created the file structure and management for my App. On the root level I created the following files index.js and package.json. Next on the public sub-folder I named it public. The following files were created home.html and about.html the structure could change as I am not sure on the theme or purpose of my app yet. Reminder this is a working in progress app that might change as the course continues.

---
+++ workspace/index.js
@@ -0,0 +1,35 @@
+var http = require("http"), fs = require('fs');
+
+function serveStatic(res, path, contentType, responseCode){
+  if(!responseCode) responseCode = 200;
+  console.log(__dirname + path)
+  fs.readFile(__dirname + path, function(err, data){
+      if(err){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end('Internal Server Error');
+      }
+      else{
+        res.writeHead(responseCode, {'Content-Type': contentType});
+        res.end(data);
+      }
+  });
+}
+
+http.createServer(function(req,res){
+  console.log('createServer got request')
+  var path = req.url.toLowerCase();
+  switch(path) {
+    case '/': 
+      serveStatic(res, '/public/home.html', 'text/html');
+      break;
+    case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('About: Working in progress);
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('404:Page not found.');
+  }
+  
+}).listen(process.env.PORT || 3000);
+//console.log('after createServer')
