4ac13c2a1cee9a2cb8f9748021bbe9460d528e51
Solomon <solomon.ab1988@gmail.com>
2018-07-09 20:23:40 -0700

    Homework #2
    Added routes for the server Getall,delete, & add.

--- workspace/index.js
+++ workspace/index.js
@@ -1,8 +1,8 @@
 var http = require("http"), fs = require('fs');
-
+var coffee = require('./lib/coffee.js');
 function serveStatic(res, path, contentType, responseCode){
   if(!responseCode) responseCode = 200;
-  console.log(__dirname + path)
+  console.log(__dirname + path);
   fs.readFile(__dirname + path, function(err, data){
       if(err){
         res.writeHead(500, {'Content-Type': 'text/plain'});
@@ -16,7 +16,7 @@ function serveStatic(res, path, contentType, responseCode){
 }
 
 http.createServer(function(req,res){
-  console.log('createServer got request')
+  console.log('createServer got request');
   var path = req.url.toLowerCase();
   switch(path) {
     case '/': 
@@ -24,12 +24,28 @@ http.createServer(function(req,res){
       break;
     case '/about':
       res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About: Working in progress);
+      res.end('About: Working in progress');
       break;
+     case '/getall':
+       let found = coffee.getAll();
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      let results = JSON.stringify(found);
+      res.end(results);
+      break;
+      case '/delete':
+      let deleted = coffee.delete(brand);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      let delected = (deleted) ? JSON.stringify(deleted): "Not found";
+      case '/add':
+      let added = coffee.add(type.FlateWhite);
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(results);
+      break;
+   
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('404:Page not found.');
   }
   
 }).listen(process.env.PORT || 3000);
-//console.log('after createServer')
+ 
