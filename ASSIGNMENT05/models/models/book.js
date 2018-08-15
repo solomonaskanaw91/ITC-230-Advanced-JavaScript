3010398a34cf7be9f34ee652f6ebe5fccce59fed
Solomon <solomon.ab1988@gmail.com>
2018-08-15 13:04:31 -0700

    Add files via upload

---
+++ ASSIGNMENT05/models/models/book.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+// remote db connection settings. 
+var connectionString = "mongodb://solomon:asasasas11@ds239911.mlab.com:39911/mongodb_sandbox";
+mongoose.connect(connectionString);
+ 
+
+
+var conn = mongoose.connection; 
+conn.on('error', console.error.bind(console, 'connection error:'));
+
+var mySchema = mongoose.Schema({
+ title: { type: String, required: true },
+ price: String, 
+ brand: String  
+
+},
+
+{collection: 'coffee' 
+
+}); 
+
+module.exports = mongoose.model('Coffee', mySchema);
